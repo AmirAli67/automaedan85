e4553907bc9cc2f2c60faeca8a4e1fcd8cd17e94
AmirAli67 <148010297+AmirAli67@users.noreply.github.com>
2023-12-21 11:40:02 +0330

    Update automaedan.py

--- automaedan.py
+++ automaedan.py
@@ -1,114 +1,78 @@
-import sys
-import requests
+from requests import post
 from time import sleep
-from uuid import uuid4
-from colorama import init, Fore
+from random import choice
+import datetime
+import sys
+from colorama import Fore
 from os import system
-import subprocess
-from json import loads
 
-init(autoreset=True)
 
-Creator = "t.me/Amir_Ali667"
-session = requests.Session()
-session.headers.update({'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 13; 23049PCD8G Build/TKQ1.221114.001)',
-                        'Accept-Encoding': 'gzip',
-                        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'})
+fruit_passport = str(input("Enter Your Fruit Passport: "))
 
-cr = Creator
-restore_key = input("Please enter your restore key: ")
 
-def decode(data):
-    final_string = ''
-    for key in data.keys():
-        final_string += f"{key}={data[key]}&"
-    return final_string[:-1]
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 10; SM-A750F Build/QP1A.190711.020)',
+    'Host': 'iran.fruitcraft.ir',
+    'Connection': 'Keep-Alive',
+    'Accept-Encoding': 'gzip',
+    'cookie': f'FRUITPASSPORT={fruit_passport}'}
 
-def load_fruitpassport(restore_key):
-    data = {'game_version': '1.7.10655',
-            'device_name': 'Samsung Galaxy S21',
-            'os_version': '10',
-            'model': 'SM-G991B',
-            'udid': str(uuid4().int),
-            'store_type': 'iraqapps',
-            'restore_key': restore_key,
-            'os_type': 2}
 
-    try:
-        response = session.post('http://iran.fruitcraft.ir/player/load', data=decode(data), timeout=5)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error making HTTP request: {e}")
-        sys.exit(1)
-    except ValueError as e:
-        print("Error decoding JSON response.")
-        sys.exit(1)
+collect_data = "edata=Gk4KXVpRXRJDSEMTfmMXSA%3D%3D"
+deposit_data = "edata=Gk4KUEFQQERbUDpPAwkBAVRZRFQ4UB4aWwoEEA5GW05bAlUECgRTQ1JIBVQEUAVdFwhSQAAJCFsDF1BRBVoMBhFJ"
 
-def collect_gold(session, data):
-    url = "http://iran.fruitcraft.ir/cards/collectgold"
-    try:
-        response = session.post(url, data=data)
-        response.raise_for_status() 
-        if response.status_code == 200:
-            return response.json()  
-        else:
-            print(f"Unexpected status code: {response.status_code}")
-            print(response.content)
-            return None
-    except requests.exceptions.RequestException as e:
-        print(f"Error making HTTP request: {e}")
-        print(str(e.response.content))
-        return None
 
-def start(sleep_time):
+
+print("\n\n\n")
+
+
+def collect_gold(data , headers):
+    #proxy = {"http" : "http://188.121.103.205:80"}
+    collect = post("http://iran.fruitcraft.ir/cards/collectgold",
+        data = data,
+        headers = headers,
+    )
+
+
+def deposit_to_bank(data , headers):
+    deposit = post("http://iran.fruitcraft.ir/player/deposittobank",
+        data = data,
+        headers = headers,
+    )
+
+
+def start():
     done = 0
     lost = 0
+    
+    for i in range(400):
+        try:
+            collect_gold(collect_data , headers)
+            if deposit_ask:
+                deposit_to_bank(deposit_data , headers)
+            done += 1
+        except Exception as e:
+            print(e)
+            lost += 1
+        finally:
+            sys.stdout.write(f"\r• Gold Mine Done: {Fore.GREEN}{str(done)}{Fore.RESET} --- • Gold Mine Lost: {Fore.RED}{str(lost)}{Fore.RESET} --- • Bank Deposit: {str(deposit_ask)}")
+            sys.stdout.flush()
+        sleep(maining_time)
 
-    try:
-        collect_gold(session, collect_data)
-        done += 1
-    except Exception as e:
-        print(e)
-        lost += 1
-    finally:
-        sys.stdout.write(
-            f"\r• Gold Mine Done: {Fore.GREEN}{str(done)}{Fore.RESET} --- • Gold Mine Lost: {Fore.RED}{str(lost)}{Fore.RESET}")
-        sys.stdout.flush()
-        sleep(sleep_time * 60)  
 
-        while True:
-            sleep(sleep_time * 60)  
-            try:
-                collect_gold(session, collect_data)
-                done += 1
-            except Exception as e:
-                print(e)
-                lost += 1
-            finally:
-                sys.stdout.write(
-                    f"\r• Gold Mine Done: {Fore.GREEN}{str(done)}{Fore.RESET} --- • Gold Mine Lost: {Fore.RED}{str(lost)}{Fore.RESET}")
-                sys.stdout.flush()
+system("clear")
+power = input('how may is your manner power ? >> ') # قدرت منفعت معدن
+capacity = input('how may is your Capacity ? >> ') #ظرفیت معدن
+deposit_ask = input('Do you want to deposit money in the bank? (Y or N) >> ') #ذخیره توی بانک؟
+deposit_ask = True if deposit_ask.lower() == "y" else False
+    
 
-def print_banner():
-    system("figlet AmirAli67 | lolcat")
-    print(Fore.YELLOW + "----------------------" + Fore.RESET)
+maining_time = int((int(capacity) / (int(power) / 3600)))
 
-def main():
-    print_banner()
-    sleep_time = int(input("Please enter the mine time (in minutes): "))
-    print(f"\n\n\n\n mine time is >> {Fore.CYAN}{sleep_time}{Fore.RESET} min \n")
-    start(sleep_time)
+print("\n\n\n\n" , 'mine time is >> ', Fore.CYAN , maining_time, Fore.RESET , ' sec' , "\n")
 
-collect_data = {'udid': str(uuid4().int), 'restore_key': restore_key}
 
-try:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "colorama"])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "requests"])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "lolcat"])
-except subprocess.CalledProcessError as e:
-    print(f"Error installing required packages: {e}")
-    sys.exit(1)
 
-load_fruitpassport(restore_key)
-main()
+start()
+
