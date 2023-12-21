2a0f293e9a071fe7ceca29d8bd0d7962506fd01c
AmirAli67 <148010297+AmirAli67@users.noreply.github.com>
2023-12-21 09:06:50 +0330

    Update automaedan.py

--- automaedan.py
+++ automaedan.py
@@ -5,8 +5,9 @@ from uuid import uuid4
 from colorama import init, Fore
 from os import system
 import subprocess
+from json import loads
 
-init(autoreset=True)  # initialize colorama
+init(autoreset=True)
 
 Creator = "t.me/Amir_Ali667"
 session = requests.Session()
@@ -18,19 +19,24 @@ cr = Creator
 restore_key = input("Please enter your restore key: ")
 
 
+def decode(data):
+    final_string = ''
+    for key in data.keys():
+        final_string += f"{key}={data[key]}&"
+    return final_string[:-1]
+
+
 def load():
-    data = {
-        'game_version': '1.7.10655',
-        'device_name': 'unknown',
-        'os_version': '10',
-        'model': 'SM-A750F',
-        'udid': str(uuid4().int),
-        'store_type': 'iraqapps',
-        'restore_key': restore_key,
-        'os_type': 2
-    }
+    data = {'game_version': '1.7.10655',
+            'device_name': 'Samsung Galaxy S21',
+            'os_version': '10',
+            'model': 'SM-G991B',
+            'udid': str(uuid4().int),
+            'store_type': 'iraqapps',
+            'restore_key': restore_key,
+            'os_type': 2}
     try:
-        response = session.post('http://iran.fruitcraft.ir/player/load', data=data, timeout=5)
+        response = session.post('http://iran.fruitcraft.ir/player/load', data=decode(data), timeout=5)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
@@ -45,9 +51,9 @@ def collect_gold(session, data):
     url = "http://iran.fruitcraft.ir/cards/collectgold"
     try:
         response = session.post(url, data=data)
-        response.raise_for_status()  # Raise an exception for 4xx/5xx status codes
+        response.raise_for_status() 
         if response.status_code == 200:
-            return response.json()  # Return JSON response if request is successful
+            return response.json()  
         else:
             print(f"Unexpected status code: {response.status_code}")
             return None
@@ -70,10 +76,10 @@ def start(sleep_time):
         sys.stdout.write(
             f"\r• Gold Mine Done: {Fore.GREEN}{str(done)}{Fore.RESET} --- • Gold Mine Lost: {Fore.RED}{str(lost)}{Fore.RESET}")
         sys.stdout.flush()
-        sleep(sleep_time * 60)  # login once after starting
+        sleep(sleep_time * 60)  
 
         while True:
-            sleep(sleep_time * 60)  # sleep for the specified time in minutes
+            sleep(sleep_time * 60)  
             try:
                 collect_gold(session, collect_data)
                 done += 1
@@ -98,7 +104,6 @@ def main():
     start(sleep_time)
 
 
-# collect_data
 collect_data = {'udid': str(uuid4().int), 'restore_key': restore_key}
 
 try:
@@ -109,5 +114,5 @@ except subprocess.CalledProcessError as e:
     print(f"Error installing required packages: {e}")
     sys.exit(1)
 
+load()  # اینجا load فراخوانی شده است
 main()
-
